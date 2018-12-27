f1826f7dc87befc1596003d887d17e98cfdca916
MrZHLF <480970024@qq.com>
2018-12-27 11:26:15 +0800

    修改个人信息界面

--- client/src/views/information/InfoShow.vue
+++ client/src/views/information/InfoShow.vue
@@ -1,24 +1,100 @@
 <template>
   <div class="infoshow">
-    <el-row type='flex' class="row-bg" justify="center">
-        <el-col :span="8">
-            <div class="user">
-                <img :src="users.avatar" alt="" class="avatar">
-            </div>
-        </el-col>
-        <el-col :sapn="16">
-            <div class="userinfo">
-                <div class="user-item">
-                    <i class="el-icon-service"></i>
-                    <span>{{users.name}}</span>
+    <div class="users">
+        <el-row>
+            <el-col :span="8">
+                <div class="box-card">
+                    <div class="el-card mgb20 is-hover-shadow" style="height: 252px">
+                        <div class="user-info">
+                            <img :src="users.avatar" alt="" class="avatar">
+                            <div class="user-info-cont">
+                                <span class="user-name">{{users.name}}</span>
+                                <span>{{users.identity == 'manager' ? '管理员' : '普通员工'}}</span>
+                            </div>
+                        </div>
+                        <div class="user-info-list">
+                            <span>登录地址:</span>
+                            <span class="address">郑州</span>
+                        </div>
+                    </div>
                 </div>
-                <div class="user-item">
-                    <i class="el-icon-setting"></i>
-                    <span>{{users.identity == 'manager' ? '管理员' : '普通员工'}}</span>
+                <!-- 语言描述 -->
+                <div class="box-card" style="margin-top: 20px">
+                    <div class="el-card is-hover-shadow" style="height: 252px">
+                        <div class="el-card__header">
+                            <span>语言描述</span>
+                        </div>
+                        <div class="el-card__body">
+                            <div class="card-body-list">
+                                <div class="title">JavaScript:</div>
+                                <div class="progress"><el-progress :percentage="70"></el-progress></div>
+                            </div>
+                            <div class="card-body-list">
+                                <div class="title">Html:</div>
+                                <el-progress class="progress" :percentage="80" color="#8e71c7"></el-progress>
+                            </div>
+                            <div class="card-body-list">
+                                <div class="title">Css:</div>
+                                <el-progress class="progress" :percentage="80" color="#8e71c7"></el-progress>
+                            </div>
+                            <div class="card-body-list">
+                                <div class="title">Vue:</div>
+                                <el-progress class="progress" :percentage="60" status="success"></el-progress>
+                            </div>
+                            <div class="card-body-list">
+                                <div class="title">Node.js:</div>
+                                <el-progress class="progress" :percentage="20" status="exception"></el-progress>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </el-col>
-    </el-row>
+            </el-col>
+            <el-col :span="16">
+                <el-row :gutter="20">
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{padding: '0px'}">
+                            <div class="grid-content grid-con-1">
+                                <i class="el-icon-view grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">1234</div>
+                                    <div>用户访问量</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{padding: '0px'}">
+                            <div class="grid-content grid-con-2 grid-con-1">
+                                <i class="el-icon-message grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">321</div>
+                                    <div>系统消息</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{padding: '0px'}">
+                            <div class="grid-content grid-con-3">
+                                <i class="el-icon-goods grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">5000</div>
+                                    <div>数量</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                </el-row>
+                <!-- 2019年 -->
+                <el-card class="box-card">
+                  <div slot="header" class="clearfix">
+                    <span>2019年目标</span>
+                    <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+                  </div>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
   </div>
 </template>
 
@@ -28,44 +104,122 @@ export default {
   name: 'infoshow',
     computed:{
         users() {
-          return this.$store.getters.user
+          return this.$store.getters.user;
         }
     },
+    created(){
+         // console.log(this.$store.getters.user)
+    }
 }
 </script>
 <style scoped>
-.infoshow {
-    width: 90%;
-    height: 100%;
-    box-sizing: border-box;
-    float: right
-}
-.row-bg {
-    width: 100%;
-    height: 100%;
-}
-.user{
-   text-align: center;
-   position: relative;
-   top: 30%; 
-}
-.user .avatar{
-    width: 150px;
-    border-radius: 50%;
-}
-.userinfo {
-    width: 100%;
-    height: 100%;
-    background: #eeeeee;
-}
-.user-item {
-    position: relative;
-    top: 30%;
-    padding: 26px;
-    font-size: 30px;
-    color: #333;
-}
-.user-item span {
-    padding-left: 10px;
-}
+    .infoshow {
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        float: right;
+        background: #eeeeee;
+    }
+    .row-bg {
+        width: 100%;
+        height: 100%;
+    }
+    .users{
+        margin: 20px;
+    }
+    .user-info {
+        display: flex;
+        align-items: center;
+        padding: 20px;
+        border-bottom: 2px solid #ccc;
+        margin-bottom: 20px;
+    }
+    .avatar{
+        width: 120px;
+        height: 120px;
+        border-radius: 50%;
+    }
+    .el-card{
+        margin-bottom: 20px;
+    }
+    .user-info-cont {
+        flex: 1;
+        padding-left: 50px;
+    }
+    .user-info-cont span {
+        display: block;
+    }
+    .user-info-cont span:first-child{
+        color: #222;
+        font-size: 22px;
+    }
+    .user-info-cont span:last-child{
+        color: #999;
+        font-size: 14px;
+        padding-top: 10px;
+    }
+    .user-info-list {
+        font-size: 14px;
+        line-height: 25px;
+        color: #999;
+        padding-left: 10px;
+    }
+    .address {
+        margin-left: 70px;
+    }
+    .card-body-list {
+        width: 100%;
+        height: 100%;
+        height: 30px;
+    }
+    .card-body-list .title {
+        width: 20%;
+        float: left
+    }
+    .card-body-list .progress{
+        width: 80%;
+        float: right;
+    }
+    .grid-content  {
+        display: flex;
+        align-items: center;
+        height: 100px;
+    }
+    .grid-con-icon {
+        font-size: 50px;
+        width: 100px;
+        height: 100px;
+        text-align: center;
+        line-height: 100px;
+        color: #fff;
+    }
+    .grid-con-1 .grid-con-icon {
+        background: #2d8cf0;
+    }
+    .grid-con-2 .grid-con-icon{
+        background: #64d572;
+    }
+    .grid-con-3 .grid-con-icon {
+        background: #f25e43;
+    }
+    .grid-cont-right {
+        flex: 1;
+        text-align: center;
+        font-size: 14px;
+        color: #999;
+    }
+    .grid-num {
+        font-size: 30px;
+        font-weight: bold;
+    }
+    .grid-con-1 .grid-num{
+        color: #2d8cf0;
+    }
+    .grid-con-3 .grid-num{
+        color: #f25e43;
+    }
+    .el-col-16 {
+        width: 65%;
+        margin-left: 1%;
+    }
 </style>
